df287d0ccbbb3e9303d499091e552be54b9c3564
Heck_ClearVision_ <abdxllahmazharx@gmail.com>
2026-04-23 23:59:34 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,18 +1,1 @@
-interfrace = int(input("Calculator: Enter First Number: "))
-num2 = int(input("Calculator: Enter second Number: "))
-
-add = str(input("Do You want to Add these 2 Numbers? (Yes or No): ")).lower()
-if add == "yes":
-    print(interfrace ,"+", num2, "=", interfrace + num2)
-else:
-    subtract = str(input("Do You want to Subtract these 2 Numbers? (Yes or No): ")).lower()
-if subtract == "yes":
-        print(interfrace ,"-", num2, "=", interfrace - num2)
-else:
-    multiply = str(input("Do You want to Multiply these 2 Numbers? (Yes or No): ")).lower()
-if multiply == "yes":
-        print(interfrace ,"x", num2, "=", interfrace * num2)
-else:
-     divide = str(input("Do You want to Divide these 2 Numbers? (Yes or No): ")).lower()
-if multiply == "yes":
-        print(interfrace ,"/", num2, "=", interfrace / num2)
+wait
