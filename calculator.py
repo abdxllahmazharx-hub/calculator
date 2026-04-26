e892fc917f0c362c1a6070696dcd3f1117456072
Heck_ClearVision_ <abdxllahmazharx@gmail.com>
2026-04-26 14:12:07 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import math
+
 def add():
     print(num1, "+", num2, "=", num1 + num2)
 
@@ -10,7 +12,15 @@ def multiply():
 def divide():
     print(num1, "/", num2, "=", num1 / num2)
 
-interface = int(input("Choose an Option from the Following List:\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\nEnter (1, 2, 3 or 4): "))
+def power():
+    print(num1, "^", num2, "=", num1 * num1)
+
+def sqroot():
+    print("Square Root of", num1, "=", math.sqrt(num1) )
+
+# cube root function is in the code line 61 (i saved time)
+
+interface = int(input("Choose an Option from the Following List:\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Power\n6. Square Root\n7. Cube Root\nEnter (1, 2, 3, 4, 5, 6 or 7): "))
 if interface == 1:
     num1 = int(input("Enter the First Number: "))
     num2 = int(input("Enter the Second Number: "))
@@ -33,6 +43,22 @@ elif interface == 4:
     num2 = int(input("Enter the Second Number: "))
 
     divide()
+
+elif interface == 5:
+    num1 = int(input("Enter the First Number: "))
+    num2 = int(input("Enter a value of Power: "))
+
+    power()
+
+elif interface == 6:
+    num1 = int(input("Enter Number: "))
+
+    sqroot()
+
+elif interface == 7:
+    num1 = int(input("Enter Number: "))
+
+    print("Cube Root of", num1, "=", math.cbrt(num1) )
 
 else:
     print("Invaild Option!")
